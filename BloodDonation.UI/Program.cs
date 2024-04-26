d8cb65f70389788a1b0065ece98136c2c1a29194
kareman <kareman.mahmoud@sewedy.com.eg>
2024-04-26 23:19:41 +0300

    add injectUtillties

--- BloodDonation.UI/Program.cs
+++ BloodDonation.UI/Program.cs
@@ -1,6 +1,12 @@
+using BloodDonation.Infrastructure.Context;
+using BloodDonation.UI.Utilities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddInjectUtility();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -12,6 +18,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
---
+++ BloodDonation.UI/Utilities/InjectionsUtility.cs
@@ -0,0 +1,29 @@
+﻿
+using BloodDonation.Infrastructure.IRepository;
+using BloodDonation.Infrastructure.Repository;
+using BloodDonation.Service.IService;
+using BloodDonation.Service.Service;
+
+namespace BloodDonation.UI.Utilities;
+public static class InjectionsUtility
+{
+    public static IServiceCollection AddInjectUtility(this IServiceCollection services)
+    {
+
+        #region Repositories
+        services.AddTransient<IBloodRequestRepository, BloodRequestRepository>();
+        services.AddTransient<IBloodRequestDonorRepository, BloodRequestDonorRepository>();
+        services.AddTransient<IHealthHistorySurveyRepository, HealthHistorySurveyRepository>();
+        services.AddTransient<IUserRepostiroy, UserRepostiroy>();
+        #endregion
+
+        #region Services
+        services.AddScoped<IBloodRequestService, BloodRequestService>();
+        services.AddScoped<IBloodRequestDonorService, BloodRequestDonorService>();
+        services.AddScoped<IHealthHistorySurveyService, HealthHistorySurveyService>();
+        services.AddScoped<IUserService, UserService>();
+        #endregion
+
+        return services;
+    }
+}
